5d981f200bfed3425a7024084ff8abc39fb45eda
JonathanPuxler <software@puxler.de>
2021-11-06 13:37:58 +0100

    Initial Projekt

---
+++ src/test/java/de/puxler/Kochbuch/KochbuchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.puxler.Kochbuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KochbuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
